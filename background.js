f8e1a4dd1984d89e9bba45be8d6b3c5ba5454ff4
Cheuk Shu Ho <72436714+2246250622@users.noreply.github.com>
2024-11-25 01:59:51 +0000

    background

---
+++ background.js
@@ -0,0 +1,6 @@
+// background.js
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Galxe Auto Complete Tasks extension installed.');
+});
+
+// You can add more event listeners or functions here as needed
